eb15df05cb27b5049e1aa8c119d5259db28410a8
Jannik Becher <becher.jannik@gmail.com>
2025-01-10 17:47:36 +0100

    Initial commit

---
+++ lib/node_type.ex
@@ -0,0 +1,18 @@
+defmodule NodeType do
+  @moduledoc """
+  Documentation for `NodeType`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NodeType.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/node_type_test.exs
@@ -0,0 +1,8 @@
+defmodule NodeTypeTest do
+  use ExUnit.Case
+  doctest NodeType
+
+  test "greets the world" do
+    assert NodeType.hello() == :world
+  end
+end
